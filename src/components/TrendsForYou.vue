476cb30db3b11d33b749e062a5149c457d329be4
DJMasaru <otsusyarepapa@gmail.com>
2022-10-07 12:29:17 +0900

    コンポーネント化予定データ作成

---
+++ src/components/TrendsForYou.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  trendsname: string,
+  tweetcount: string,
+})
+</script>
+
+<template>
+  <div class="trends-contents">
+              <div class="trends-line1">
+                <div>Trending in Japan</div>
+                <div>...</div>
+              </div>
+              <div class="trends-line2">
+                {{trendsname}}
+              </div>
+              <div class="trends-line3">
+                {{tweetcount}} Tweets
+              </div>
+            </div>
+</template>
+
+<style scoped>.trends-line1{
+  font-size: 13px;
+  color: gray;
+  display: flex;
+  justify-content: space-between;
+}
+.trends-line2{
+  font-size: 15px;
+}
+.trends-line3{
+  font-size: 13px;
+  color: gray;
+}
+.trends-contents{
+  margin-top: 20px;
+}
+</style>
+
+
+
