2b93f926c0c19717f0dfcd975924858f8eb18fc4
mikana0918 <shogo.kusuhara@gmail.com>
2022-10-16 13:48:54 +0900

    feat: Add tweet and fav feature

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -10,4 +10,25 @@ export const tweet = {
       console.error(error);
     }
   },
+  create: async (payload) => {
+    const { body } = payload;
+    try {
+      const { data } = await axios.post(`/api/tweets`, { body });
+
+      return data.tweet;
+    } catch (error) {
+      console.error(error);
+
+      return null;
+    }
+  },
+  favoriteById: async (tweetId) => {
+    try {
+      await axios.post(`/api/tweets/favorites/${tweetId}`);
+    } catch (error) {
+      console.error(error);
+
+      return null;
+    }
+  },
 };
